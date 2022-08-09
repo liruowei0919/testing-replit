f9503a549967c9accc616cb96c6fa5b21d25fc84
李 <>
2022-08-09 03:52:41 +0000

    added assignment

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ __author__ = 'Oliver Banse'
 # IDEA
 # Import and Initialization
 import pygame
-
+import numpy as np
 from classes.Game import Game
 from classes.Screen import Screen
-
+print("the game starts!")
 pygame.init()
+#write here
+
+#
+
+
 
 # Display configuration
 screen = Screen('Hit the Mole!', 800, 600)
